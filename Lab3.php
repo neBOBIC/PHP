b3897edea2623a9acc14fcc6b5b56dc5c47cc449
neBOBIC <adriancik6@gmail.com>
2025-09-19 08:52:23 +0300

    perimeter and surface

--- Lab3.php
+++ Lab3.php
@@ -1,3 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-?>
+    <form method="post">
+        <label for="latime">Introdu latimea: </label>
+        <input type="text" name="latime" id="latime">
+        <br>
+        <label for="lungime">Introdu lungimea</label>
+        <input type="text" name="lungime" id="lungime">
+        <br>
+        <button type="submit">Calculeaza</button>
+    </form>
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $lat = $_POST["latime"];
+        $lng = $_POST["lungime"];
+        $perimetru = 2 * ($lat + $lng);
+        $aria = $lat * $lng;
+        
+        echo "<p>Perimetrul este: $perimetru</p>";
+        echo "<p>Aria este: $aria</p>";
+    }
+    ?>
+</body>
+</html>
